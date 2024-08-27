84044a990d378c3c6d7129545ecf227bac529116
ultimate-patronum <108108200+ultimate-patronum@users.noreply.github.com>
2024-08-27 12:44:44 -0700

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,6 +9,11 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
+    let totalCost = 0;
+    plantItems.forEach((item) => {
+        totalCost += item.quantity * item.cost;
+    });
+    return totalCost;
  
   };
 
@@ -16,13 +21,20 @@ const CartItem = ({ onContinueShopping }) => {
    
   };
 
-
+    const handleCheckoutShopping = (e) => {
+        alert('Functionality to be added for future reference');
+    };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity +1}));
   };
 
   const handleDecrement = (item) => {
-   
+   if(item.quantity >1){
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity-1}));
+   }else{
+    dispatch(removeItem({name:item.name}));
+   }
   };
 
   const handleRemove = (item) => {
@@ -30,6 +42,7 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    cost = item.quantity * item.price;
   };
 
   return (
