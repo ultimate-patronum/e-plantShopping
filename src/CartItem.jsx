7189277f312b363f447bc23289667a34f3e1c3af
ultimate-patronum <108108200+ultimate-patronum@users.noreply.github.com>
2024-08-28 20:19:04 -0700

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -38,6 +38,7 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem({name:item.name}));
   };
 
   // Calculate total cost based on quantity for an item
