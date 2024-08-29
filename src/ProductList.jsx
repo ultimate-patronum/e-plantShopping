19e002a628c178ebc166fcaeb8f0ce97bafdc816
ultimate-patronum <108108200+ultimate-patronum@users.noreply.github.com>
2024-08-28 20:19:41 -0700

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,10 +2,14 @@ import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
+import { useDispatch } from 'react-redux';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = useDispatch();
+    const cartItems = useSelector(state => state.cart.items);
+    
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -233,7 +237,7 @@ function ProductList() {
     fontSize: '30px',
     textDecoration: 'none',
    }
-   const handleCartClick = (e) => {
+const handleCartClick = (e) => {
     e.preventDefault();
     setShowCart(true); // Set showCart to true when cart icon is clicked
 };
@@ -242,18 +246,21 @@ const handlePlantsClick = (e) => {
     setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
     setShowCart(false); // Hide the cart when navigating to About Us
 };
-
-   const handleContinueShopping = (e) => {
+const handleContinueShopping = (e) => {
     e.preventDefault();
     setShowCart(false);
   };
-  const handleAddToCart = (product) => {
+const handleAddToCart = (product) => {
     dispatch(addItem(product));
     setAddedToCart((prevState) => ({
        ...prevState,
        [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
      }));
   };
+const getQuantity = (name) =>{
+    const item = cartItems.find(item => item.name === name);
+    return item ? item.quantity : 0;
+  }
     return (
         <div>
              <div className="navbar" style={styleObj}>
